5150fc1439d0a735ed367d4ff60400e3e58c23f9
Artem <riddle1994@ya.ru>
2022-05-10 12:56:51 +0300

    fixed postFragment

--- app/src/main/java/ru/netology/nmedia/activity/PostFragment.kt
+++ app/src/main/java/ru/netology/nmedia/activity/PostFragment.kt
@@ -1,10 +1,12 @@
 package ru.netology.nmedia.activity
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.PopupMenu
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
@@ -26,14 +28,30 @@ class PostFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View = PostFragmentBinding.inflate(inflater, container, false).also { binding ->
 
-        var post: Post = requireArguments().getParcelable(POST_KEY)!!
+        var post: Post = requireNotNull(arguments?.getParcelable(POST_KEY))
         val listener = viewModel
         val holder = PostViewHolder(binding.postCell, listener)
         holder.bind(post)
 
         viewModel.data.observe(viewLifecycleOwner) { posts ->
-            posts.find { s -> s.id == post.id }?.let { holder.bind(it) }
+            posts.find { s -> s.id == post.id }?.let { holder.bind(it) } ?: findNavController().navigateUp()
         }
+
+        viewModel.navigateToEditPostScreen.observe(viewLifecycleOwner) { editing ->
+            val result = bundleOf(EditPostFragment.EDITING_POST_KEY to editing)
+            findNavController().navigate(R.id.toEditPostFragment, result)
+        }
+
+        viewModel.shareEvent.observe(viewLifecycleOwner) { postContent ->
+            val intent = Intent().apply {
+                action = Intent.ACTION_SEND
+                type = "text/plain"
+                putExtra(Intent.EXTRA_TEXT, postContent)
+            }
+            val shareIntent = Intent.createChooser(intent, getString(R.string.chooser_share_post))
+            startActivity(shareIntent)
+        }
+
     }.root
 
 
